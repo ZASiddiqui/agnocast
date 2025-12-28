bdb3c384110f5692d59a1603ebf6a924a598be32
Zaryan Ahmed Siddiqui <zaryan16037@gmail.com>
2025-12-28 22:58:37 +0500

    fix: Corrected the parameters of decrement_rc function call in agnocast_callback_info.cpp
    Signed-off-by: Zaryan Ahmed Siddiqui <zaryan16037@gmail.com>

--- src/agnocastlib/src/agnocast_callback_info.cpp
+++ src/agnocastlib/src/agnocast_callback_info.cpp
@@ -62,13 +62,13 @@ void receive_message(
       if(ready_agnocast_executables.size() < 5){
       std::lock_guard<std::mutex> ready_lock{ready_agnocast_executables_mutex};
       ready_agnocast_executables.emplace_back(
-        AgnocastExecutable{callable, callback_info.callback_group, receive_args.ret_entry_ids[i]});
+        AgnocastExecutable{callable, callback_info.callback_group, receive_args.ret_entry_ids[i],callback_info});
     }
     else{
-      ready_agnocast_executables.erase(ready_agnocast_executables.begin());
       agnocast::decrement_rc(
-        callback_info.topic_name, callback_info.subscriber_id,
-        receive_args.ret_entry_ids[i]);
+        ready_agnocast_executables[0].callback_info.topic_name, ready_agnocast_executables[0].callback_info.subscriber_id,
+        ready_agnocast_executables[0].entry_id);
+      ready_agnocast_executables.erase(ready_agnocast_executables.begin());
     }
   }
   }
